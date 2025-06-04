7f18bc222eade9a52be8644d0fc7b37badc4ffbe
guilherme goncalves <guilherm3goncalves@hotmail.com>
2025-06-04 17:47:30 -0300

    Final version

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
   collectCoverageFrom: ['src/**/*.(t|j)s'],
   coverageDirectory: './coverage',
   testEnvironment: 'node',
